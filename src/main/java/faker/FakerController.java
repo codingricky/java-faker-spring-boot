b60a62b0acddde7d848d84c384a4bc022dfef5c2
Ricky <ricky@dius.com.au>
2019-07-20 20:57:07 +1000

    added locale choice

--- src/main/java/faker/FakerController.java
+++ src/main/java/faker/FakerController.java
@@ -3,24 +3,32 @@ package faker;
 import com.github.javafaker.Faker;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Locale;
 import java.util.stream.Collectors;
 
 @Controller
 public class FakerController {
-    @GetMapping("/")
-    public String index(Model model) {
-        Faker faker = new Faker();
+    @RequestMapping(value = "/", method = {RequestMethod.GET})
+    public String index(@RequestParam(defaultValue="US") String locale, Model model) {
+        model.addAttribute("locales", Arrays.asList(Locale.US.getCountry(),
+                Locale.FRANCE.getCountry(),
+                Locale.GERMANY.getCountry()));
+
+        Faker faker = new Faker(new Locale(locale));
         List<FakerObject> fakerObjects = Arrays.asList(faker.name(),
                 faker.address(),
                 faker.phoneNumber(),
                 faker.ancient(),
                 faker.app(),
                 faker.artist(),
-                faker.avatar()).stream().map(FakerController::convertTo).collect(Collectors.toList());
+                faker.avatar(),
+                faker.chuckNorris(),
+                faker.harryPotter(),
+                faker.superhero()).stream().map(FakerController::convertTo).collect(Collectors.toList());
         model.addAttribute("values", fakerObjects);
         return "faker.html";
     }
